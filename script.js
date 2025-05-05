0ac1322647de6a0ed5b9a43ac0b95e674d55e9f5
GAUTAMVERM <89487056+GAUTAMVERM@users.noreply.github.com>
2025-05-05 19:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function initialPromise() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+function updateOutput(array) {
+  document.getElementById('output').textContent = array.toString();
+}
+
+initialPromise()
+  .then((array) => {
+    const evenNumbers = array.filter(num => num % 2 === 0);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        updateOutput(evenNumbers);
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then((evenNumbers) => {
+    const doubled = evenNumbers.map(num => num * 2);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        updateOutput(doubled);
+        resolve();
+      }, 2000);
+    });
+  });
